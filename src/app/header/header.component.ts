1964fb70507a0bd7b2dff51f7e055183750ac6c3
senthilkumarjp <96403664+senthilkumarjp@users.noreply.github.com>
2022-03-19 17:14:54 +0530

    Add files via upload

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent implements OnInit, OnDestroy {
+  selected = 'option2';
+  userIsAuthenticated = false;
+  private authListenerSubs: Subscription;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authListenerSubs = this.authService
+      .getAuthStatusListener()
+      .subscribe((isAuthenticated) => {
+        this.userIsAuthenticated = isAuthenticated;
+      });
+  }
+
+  onLogOut() {
+    this.authService.logout();
+  }
+
+  ngOnDestroy(): void {}
+}
